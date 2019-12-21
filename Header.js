4d8398631e4e56479d42ea9c160585fe6971869f
TheVersee <sainivinayak1999@gmail.com>
2019-12-21 13:25:09 +0530

    Initial commit

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+function Header(){
+  return (
+    <div>
+      <div className="hdr"><h2 className="hdr-content">Weather App</h2></div>
+      
+    </div>
+    
+  )
+}
+/*<div className="pby"><p>Powered By Saini</p></div>*/
+export default Header;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-verse
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-verse)
